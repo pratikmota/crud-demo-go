985c04ccb11dc98187dd47057778d83f81bfe038
Pratik Mota <mota.pratik@gmail.com>
2023-10-04 13:27:40 +0530

    post api change

--- short-url/handle/handle.go
+++ short-url/handle/handle.go
@@ -1,13 +1,22 @@
 package handle
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 )
 
+// POST
 func ShortUrl(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Hello")
+	w.Header().Set("Content-Type", "application/json")
+	var req ShortRequest
+	_ = json.NewDecoder(r.Body).Decode(&req)
+	json.NewEncoder(w).Encode(&req)
 }
+
+// GET
 func RedirectURL(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("Hello")
+	//
+	//https://dev.to/envitab/how-to-build-a-url-shortener-with-go-5hn5
 }
